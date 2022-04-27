c83a8d00f378b930c71c520101987be76831aeba
Mamiyev <yuska19.99@mail.ru>
2022-04-27 18:07:22 +0300

    edit document structure, add fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Ratings App</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
